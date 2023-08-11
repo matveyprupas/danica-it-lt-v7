b5dae3c5f1f9d2fc194964be59d8bc2463074d59
matveyprupas <matveyprupas@gmail.com>
2023-08-11 13:19:02 +0300

    feat: add TS, eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'jest': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'react-hooks',
+        '@typescript-eslint',
+        'prettier'
+    ],
+    'rules': {
+        'react/react-in-jsx-scope': 'off',
+        'camelcase': 'error',
+        'quotes': ['error', 'single'],
+        'no-duplicate-imports': 'error'
+    },	
+    'settings': {
+        'import/resolver': {
+          'typescript': {}
+        }
+      }
+}
