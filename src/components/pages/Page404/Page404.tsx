491c330ca6fb2d4f6f19275a2fad09d8e75a68e9
matveyprupas <matveyprupas@gmail.com>
2023-08-16 01:53:34 +0300

    fix: fix page 404

--- src/components/pages/Page404/Page404.tsx
+++ src/components/pages/Page404/Page404.tsx
@@ -2,8 +2,8 @@ import './Page404.scss';
 
 export function Page404() {
   return (
-    <div className="film-list">
-      Page not found :(
+    <div className="page404">
+      <h1>Page not found :(</h1>
     </div>
   );
 }
